e9281b61370cbfe3ad569126b9fe66e3f3dcd8e6
Ankur Sardar <ankursardar.work@gmail.com>
2025-12-26 11:33:19 +0530

    array js

---
+++ JS/Module2/Symbol.js
@@ -0,0 +1,4 @@
+// each symbol has uniqueness irrespective of the same description
+let sym1 =Symbol("Holaa")
+let sym2 =Symbol("Holaa")
+console.log(sym1===sym2)
---
+++ JS/Module3/Array.js
@@ -0,0 +1,8 @@
+//approach 1
+
+const arr1=["hello",'bye','tata']
+
+//approach 2 - constructor
+const arr2 = new Array("hello","bye")
+
+console.log(arr1,arr2)
